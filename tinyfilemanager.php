e341a2befb4830b8c03906ca82bfb302cda87a97
RobcioR <104667267+rrobciorr@users.noreply.github.com>
2025-02-04 20:21:26 +0100

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -91,7 +91,7 @@ $allowed_file_extensions = 'txt,html,css,js,php,ttf';
 
 // Allowed file extensions for upload files
 // e.g. 'gif,png,jpg,html,txt'
-$allowed_upload_extensions = '';
+$allowed_upload_extensions = '*';
 
 // Favicon path. This can be either a full url to an .PNG image, or a path based on the document root.
 // full path, e.g http://example.com/favicon.png
