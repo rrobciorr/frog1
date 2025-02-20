584db86df04101738720dd0dc173e65063cb9f64
rrobciorr <104667267+rrobciorr@users.noreply.github.com>
2025-02-20 13:15:51 +0100

    Update conect.php adding tests

--- Stocktool/lib/conect.php
+++ Stocktool/lib/conect.php
@@ -1,5 +1,32 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
+class MarketFunctionsTest extends TestCase
+{
+    public function testSend()
+    {
+        // Ponieważ funkcja send() używa mail(), nie możemy jej przetestować bezpośrednio.
+        // Można zamiast tego przetestować, czy funkcja istnieje i można ją wywołać.
+        $this->assertTrue(function_exists('send'));
+    }
+
+    public function testGetPrices()
+    {
+        // Sprawdzamy, czy funkcja zwraca tablicę cen
+        $prices = get_prices("AAPL"); // Testujemy na popularnym tickerze
+        
+        $this->assertIsArray($prices, "Funkcja get_prices() powinna zwracać tablicę");
+        $this->assertNotEmpty($prices, "Tablica cen nie powinna być pusta");
+        
+        // Sprawdzamy, czy elementy tablicy są liczbami
+        foreach ($prices as $price) {
+            $this->assertIsFloat((float) $price, "Elementy tablicy powinny być liczbami zmiennoprzecinkowymi");
+        }
+    }
+}
+
+
 function send($tresc = "demo info ")
 {
 
