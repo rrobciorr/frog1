6ed53d89dde9d469b0adecded30dafbe0fe93cf8
rrobciorr <rrobciorr@gmail.com>
2025-02-21 15:03:25 +0100

    ook

--- Stocktool/lib/MarketFunctionsTest.php
+++ Stocktool/lib/MarketFunctionsTest.php
@@ -1,4 +1,5 @@
 <?php
+include "conect.php";
 use PHPUnit\Framework\TestCase;
 
 class MarketFunctionsTest extends TestCase
@@ -14,10 +15,10 @@ class MarketFunctionsTest extends TestCase
     {
         // Sprawdzamy, czy funkcja zwraca tablicę cen
         $prices = get_prices("AAPL"); // Testujemy na popularnym tickerze
-        
+
         $this->assertIsArray($prices, "Funkcja get_prices() powinna zwracać tablicę");
         $this->assertNotEmpty($prices, "Tablica cen nie powinna być pusta");
-        
+
         // Sprawdzamy, czy elementy tablicy są liczbami
         foreach ($prices as $price) {
             $this->assertIsFloat((float) $price, "Elementy tablicy powinny być liczbami zmiennoprzecinkowymi");
